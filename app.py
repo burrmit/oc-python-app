ca5ce2bf5548251b701589cd9b1d2cc8eb4f63a4
Mitchell Burr <mitchell.burr@sunlife.com>
2022-03-29 08:39:47 -0400

    Change app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask import render_template
+from app import app
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+#@app.route("/")
+#def index():
+#    return "Congratulations, it's a web app!"
 
 @app.route('/templates')
 def template():
     return render_template('home.html')
-
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)

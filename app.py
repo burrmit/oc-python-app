481631443c6b9784f82fa5852f54336d339b9f23
Mitchell Burr <mitchell.burr@sunlife.com>
2022-03-29 08:11:34 -0400

    Adding additional files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_templates
 
 app = Flask(__name__)
 
@@ -6,5 +7,9 @@ app = Flask(__name__)
 def index():
     return "Congratulations, it's a web app!"
 
+@app.route('/templates')
+def template():
+    return render_template('home.html')
+
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=5000)
